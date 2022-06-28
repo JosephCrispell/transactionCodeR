98774ab1a21daa11b7ebaeac7cacd953a6542225
Joseph Crispell <crispelljoseph@gmail.com>
2022-06-28 21:11:57 +0100

    Minor reformatting

--- R/analyse_transactions.R
+++ R/analyse_transactions.R
@@ -2,31 +2,37 @@
 
 # Set working directory to current script's folder
 script_folder <- dirname(rstudioapi::getSourceEditorContext()$path)
-setwd(script_folder)
+setwd(script_folder) 
 
 # Note data folder
 data_folder <- file.path("..", "data")
-
-# Load functions
-source("functions.R")
 
 # Note key parameters
 date_column <- "Transaction Date"
 date_format <- "%d/%m/%Y"
 description_column <- "Transaction Description"
 in_column <- "Credit Amount"
-out_column <- "Debit Amount"
+  out_column <- "Debit Amount"
+
+# Note input files
+transactions_file <- file.path(data_folder, "14279266_20221417_0806.csv")
+coding_file <- file.path(data_folder, "transaction_coding_dictionary.csv")
+
+# Load functions
+source("functions.R")
+
+#### Load data ####
 
 # Load the transactions
 transactions <- read.csv(
-  file.path(data_folder, "14279266_20221417_0806.csv"), 
+  transactions_file, 
   stringsAsFactors = FALSE,
   check.names = FALSE
 )
 
 # Load transaction coding dictionary
 transaction_types <- read.csv(
-  file.path(data_folder, "transaction_coding_dictionary.csv"),
+  coding_file,
   stringsAsFactors = FALSE
 )
 transaction_coding <- build_transaction_coding_dictionary(transaction_types)
@@ -65,6 +71,9 @@ frequent_unclassified_descriptions <-
 
 #### Calculate monthly statistics ####
 
+# Fix NA values - not taken into consideration when calculating average
+
+
 # Calculate total pay and costs by month
 totals_by_month <- summarise_transactions(
   transactions = transactions,
@@ -85,6 +94,7 @@ totals_by_type_and_month <- summarise_transactions(
 )
 
 # Calculate average in/out on each type
+totals_by_type_and_month[is.na(totals_by_type_and_month)] <- 0
 average_by_type <- summarise_transactions(
   transactions = totals_by_type_and_month,
   in_column = in_column,
@@ -94,7 +104,18 @@ average_by_type <- summarise_transactions(
   calculate_average = FALSE
 )
 
+# Get car associated costs
+car_types <- c("Car", "Petrol", "Car emergency")
+car_average_monthly_costs <- average_by_type[average_by_type$Type %in% car_types, ]
+
 #### Plots ####
 
 # Plot in and out payments by type through time
 plot_monthly_in_out_for_each_type(totals_by_type_and_month)
+
+# Plot only car costs
+plot_monthly_in_out_for_each_type(
+  totals_by_type_and_month[totals_by_type_and_month$Type %in% car_types, ]
+)
+
+# Get
